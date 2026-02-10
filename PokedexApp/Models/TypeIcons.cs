d2df0bd0e9e69662cf87961f99af14b0ca8a7fb5
Anders Hedström <toastwaffle@gmail.com>
2026-02-10 15:44:14 +0100

    updated type icon filenames & static class

--- PokedexApp/Models/TypeIcons.cs
+++ PokedexApp/Models/TypeIcons.cs
@@ -8,24 +8,24 @@ namespace PokedexApp.Models
     {
         private static Dictionary<PokemonType, string> _iconPaths = new()
         {
-            [PokemonType.Normal] = "",
-            [PokemonType.Fighting] = "",
-            [PokemonType.Flying] = "",
-            [PokemonType.Poison] = "",
-            [PokemonType.Ground] = "",
-            [PokemonType.Rock] = "",
-            [PokemonType.Bug] = "",
-            [PokemonType.Ghost] = "",
-            [PokemonType.Steel] = "",
-            [PokemonType.Fire] = "" ,
-            [PokemonType.Water] = "",
-            [PokemonType.Grass] = "",
-            [PokemonType.Electric] = "",
-            [PokemonType.Psychic] = "",
-            [PokemonType.Ice] = "",
-            [PokemonType.Dragon] = "",
-            [PokemonType.Dark] = "",
-            [PokemonType.Fairy] = ""
+            [PokemonType.Normal] = "typeicon_normal.png",
+            [PokemonType.Fighting] = "typeicon_fighting.png",
+            [PokemonType.Flying] = "typeicon_flying.png",
+            [PokemonType.Poison] = "typeicon_poison.png",
+            [PokemonType.Ground] = "typeicon_ground.png",
+            [PokemonType.Rock] = "typeicon_rock.png",
+            [PokemonType.Bug] = "typeicon_bug.png",
+            [PokemonType.Ghost] = "typeicon_ghost.png",
+            [PokemonType.Steel] = "typeicon_steel.png",
+            [PokemonType.Fire] = "typeicon_fire.png",
+            [PokemonType.Water] = "typeicon_water.png",
+            [PokemonType.Grass] = "typeicon_grass.png",
+            [PokemonType.Electric] = "typeicon_electric.png",
+            [PokemonType.Psychic] = "typeicon_psychic.png",
+            [PokemonType.Ice] = "typeicon_ice.png",
+            [PokemonType.Dragon] = "typeicon_dragon.png",
+            [PokemonType.Dark] = "typeicon_dark.png",
+            [PokemonType.Fairy] = "typeicon_fairy.png"
         };
 
         public static string GetIcon(PokemonType type) => _iconPaths[type];
